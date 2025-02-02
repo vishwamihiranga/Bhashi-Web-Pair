da2ba1e55e80be2d0c667d865f7638ae54b36d71
Vishwa Mihiranga <145928580+vishwamihiranga@users.noreply.github.com>
2025-02-02 08:52:52 +0545

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'EnterYourMegaEmail',
-    password: 'EnterYourMegaPassword',
+    email: 'teamchalah@gmail.com',
+    password: 'CHALAH@1228@',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
